3400a88b2d2eca6fb75d28e6207fe00dcb8b7e0e
wangr0031 <wangr0031@163.com>
2018-08-20 21:01:34 +0800

    add clean db schema

--- ops_db.py
+++ ops_db.py
@@ -3,46 +3,97 @@
 __author__ = 'wangrong'
 import cx_Oracle
 from ora_logger import logger
+import yaml
 
 
 class OpsDb():
-    def __init__(self, db_username, db_password, db_dns,con_mode=None):
+    def __init__(self, db_username, db_password, db_dns, con_mode=None):
         self.db_username = db_username
         self.db_password = db_password
         self.db_dns = db_dns
-        self.cx_con=self.connect_db(con_mode)
+        self.cx_con = self.connect_db(con_mode)
 
-    def connect_db(self, con_mode=None):
+    def connect_db(self, con_mode):
         try:
-            cx_con = cx_Oracle.connect(self.db_username, self.db_password, self.db_dns, con_mode)
+            if con_mode is not None:
+                cx_con = cx_Oracle.connect(self.db_username, self.db_password, self.db_dns, mode=con_mode)
+            else:
+                cx_con = cx_Oracle.connect(self.db_username, self.db_password, self.db_dns)
             logger.info("Connect to Database Success!")
         except Exception as err:
             logger.error("Connect to Database [username={},password={},ora_dsn={}] get error!".format(self.db_username,
-                                                                                                     self.db_password,
-                                                                                                     self.db_dns))
+                                                                                                      self.db_password,
+                                                                                                      self.db_dns))
             logger.error("error:{}".format(err))
+            exit()
         return cx_con
 
-    def insert_data(self,insert_sql):
+    def dml_data(self, db_con_instance, dml_sql, strip_word=';'):
+        db_cursor = db_con_instance.cursor()
+        try:
+            db_cursor.execute(dml_sql.strip(strip_word))
+            db_con_instance.commit()
+            logger.info("execute DML <{}> success and commit".format(dml_sql.strip(strip_word)))
+            db_cursor.close()
+        except Exception as err:
+            logger.error("Execute DML [{}] statment get error.".format(dml_sql))
+            logger.error("error info:\n{}".format(err))
+            db_con_instance.rollback()
+            db_cursor.close()
+            exit()
+
+    def ddl_data(self, db_con_instance, ddl_sql, strip_word=';'):
+        db_cursor = db_con_instance.cursor()
+        print("ddl = ", ddl_sql.strip(strip_word))
+        try:
+            logger.debug("Execute DDL = {}".format(ddl_sql))
+            # db_cursor.execute(ddl_sql.strip(strip_word))
+        except Exception as err:
+            logger.error("Execute DDL [{}] statment get error.".format(ddl_sql))
+            logger.error("error info:\n{}".format(err))
+            db_cursor.close()
+            exit()
+
+    def select_data(self, db_con_instance, select_sql, strip_word=';'):
+        db_cursor = db_con_instance.cursor()
+        try:
+            db_cursor.execute(select_sql.strip(strip_word))
+            select_result = db_cursor.fetchall()
+        except Exception as err:
+            logger.error("Execute SELECT [{}] statment get error.".format(select_sql))
+            logger.error("error info:\n{}".format(err))
+            db_cursor.close()
+            exit()
+        return select_result
+
+    def export_data_from_table(self, table_name):
         pass
 
-    def update_data(self,update_sql):
-        pass
-
-    def delete_data(self,delete_sql):
-        pass
-
-    def select_data(self,select_sql):
-        pass
-
-    def export_data_from_table(self,table_name):
-        pass
-##导出AWR报告
+    ##导出AWR报告
     def export_awr_report(self):
         pass
-##导出ASH报告
+
+    ##导出ASH报告
     def export_ash_report(self):
         pass
 
     def export_addm_report(self):
         pass
+
+    def clean_db_schema_by_user(self):
+        with open('CONF/drop_db_schema_by_user_with_sql.cfg', 'rb') as f:
+            sql_cfg = yaml.load(f)
+        for drop_type in sql_cfg['drop_obj']:
+            for onesql in sql_cfg['drop_obj'][drop_type]:
+                print("Process drop_type", drop_type)
+                logger.info("Clean DB_type {}".format(drop_type))
+                db_result_rows = self.select_data(self.cx_con, onesql)
+                if db_result_rows:
+                    for row in db_result_rows:
+                        # print ("row=",'.'.join(row))
+                        self.ddl_data(self.cx_con, '.'.join(row))
+
+
+if __name__ == '__main__':
+    o = OpsDb('cpc', '1jian8Shu!', '172.16.80.11:11521/cc')
+    o.clean_db_schema_by_user()
