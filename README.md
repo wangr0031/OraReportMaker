1c61dbd2b6183fb13c461566c2acd15750d819c2
wangr0031 <wangr0031@163.com>
2018-08-21 13:38:33 +0800

    add ora exception

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # OraReportMaker
 ## 开发日志
-### 20180819 新增程序的logger,后续日志输出统一使用自定义的logger
+1. 20180819 新增程序的logger,后续日志输出统一使用自定义的logger
+2. 20180821 DBMS_WORKLOAD_REPOSITORY.awr_report_html生成awr报告
--- ops_db.py
+++ ops_db.py
@@ -20,11 +20,20 @@ class OpsDb():
             else:
                 cx_con = cx_Oracle.connect(self.db_username, self.db_password, self.db_dns)
             logger.info("Connect to Database Success!")
-        except Exception as err:
+        except cx_Oracle.DatabaseError as oraerr:
+            #print (oraerr.args,dir(oraerr.args))
+            error, = oraerr.args
             logger.error("Connect to Database [username={},password={},ora_dsn={}] get error!".format(self.db_username,
                                                                                                       self.db_password,
                                                                                                       self.db_dns))
-            logger.error("error:{}".format(err))
+            logger.error("Oracle-Error-Code:{}".format(error.code))
+            logger.error("Oracle-Error-Message:{}".format(error.message))
+            exit()
+        except Exception as oserr:
+            logger.error("Connect to Database [username={},password={},ora_dsn={}] get error!".format(self.db_username,
+                                                                                                      self.db_password,
+                                                                                                      self.db_dns))
+            logger.error("Connection Error:{}".format(oserr))
             exit()
         return cx_con
 
@@ -35,9 +44,15 @@ class OpsDb():
             db_con_instance.commit()
             logger.info("execute DML statement <{}> success and commit".format(dml_sql.strip(strip_word)))
             db_cursor.close()
-        except Exception as err:
+        except cx_Oracle.DatabaseError as oraerr:
+            error, = oraerr.args
+            logger.error("Execute DDL statement [{}] get error.".format(dml_sql))
+            logger.error("Oracle-Error-Code:{}".format(error.code))
+            logger.error("Oracle-Error-Message:{}".format(error.message))
+            exit()
+        except Exception as oserr:
             logger.error("Execute DML statement [{}] get error.".format(dml_sql))
-            logger.error("error info:\n{}".format(err))
+            logger.error("error info:\n{}".format(oserr))
             db_con_instance.rollback()
             db_cursor.close()
             exit()
@@ -47,26 +62,40 @@ class OpsDb():
         print("ddl = ", ddl_sql.strip(strip_word))
         try:
             logger.debug("Execute DDL = {}".format(ddl_sql))
-            db_cursor.execute(ddl_sql.strip(strip_word))
-        except Exception as err:
+            #db_cursor.execute(ddl_sql.strip(strip_word))
+        except cx_Oracle.DatabaseError as oraerr:
+            error, = oraerr.args
             logger.error("Execute DDL statement [{}] get error.".format(ddl_sql))
-            logger.error("error info:\n{}".format(err))
+            logger.error("Oracle-Error-Code:{}".format(error.code))
+            logger.error("Oracle-Error-Message:{}".format(error.message))
+            exit()
+        except Exception as oserr:
+            logger.error("Execute DDL statement [{}] get error.".format(ddl_sql))
+            logger.error("error info:\n{}".format(oserr))
             db_cursor.close()
             exit()
 
     def select_data(self, db_con_instance, select_sql, strip_word=';'):
         db_cursor = db_con_instance.cursor()
         total_count = 0
+        select_result=list()
         try:
             db_cursor.execute(select_sql.strip(strip_word))
             select_result = db_cursor.fetchall()
-            total_count=db_cursor.rowcount
-        except Exception as err:
+            #print ("type,",type(select_result))
+            total_count = db_cursor.rowcount
+        except cx_Oracle.DatabaseError as oraerr:
+            error, = oraerr.args
+            logger.error("Execute DDL statement [{}] get error.".format(select_sql))
+            logger.error("Oracle-Error-Code:{}".format(error.code))
+            logger.error("Oracle-Error-Message:{}".format(error.message))
+            exit()
+        except Exception as oserr:
             logger.error("Execute SELECT statement [{}] get error.".format(select_sql))
-            logger.error("error info:\n{}".format(err))
+            logger.error("error info:\n{}".format(oserr))
             db_cursor.close()
             exit()
-        return select_result,total_count
+        return select_result, total_count
 
     def export_data_from_table(self, table_name):
         pass
@@ -88,8 +117,8 @@ class OpsDb():
         for drop_type in sql_cfg['drop_obj']:
             for onesql in sql_cfg['drop_obj'][drop_type]:
                 print("Process drop_type", drop_type)
-                db_result_rows,total_rows = self.select_data(self.cx_con, onesql)
-                logger.info("Clean DB_type {},Total counts {}".format(drop_type,total_rows))
+                db_result_rows, total_rows = self.select_data(self.cx_con, onesql)
+                logger.info("Clean DB_type {},Total counts {}".format(drop_type, total_rows))
                 if db_result_rows:
                     for row in db_result_rows:
                         # print ("row=",'.'.join(row))
@@ -97,5 +126,5 @@ class OpsDb():
 
 
 if __name__ == '__main__':
-    o = OpsDb('cpc', '1jian8Shu!', '172.16.80.11:11521/cc')
+    o = OpsDb('cpc_flow', '1jian8Shu', '172.16.80.11:11521/cc')
     o.clean_db_schema_by_user()
