3d418d2bcede0ca266367515d68a1c6fab8d02cc
wangr0031 <wangr0031@163.com>
2018-08-21 09:28:28 +0800

    show total count when select statement

--- ops_db.py
+++ ops_db.py
@@ -33,10 +33,10 @@ class OpsDb():
         try:
             db_cursor.execute(dml_sql.strip(strip_word))
             db_con_instance.commit()
-            logger.info("execute DML <{}> success and commit".format(dml_sql.strip(strip_word)))
+            logger.info("execute DML statement <{}> success and commit".format(dml_sql.strip(strip_word)))
             db_cursor.close()
         except Exception as err:
-            logger.error("Execute DML [{}] statment get error.".format(dml_sql))
+            logger.error("Execute DML statement [{}] get error.".format(dml_sql))
             logger.error("error info:\n{}".format(err))
             db_con_instance.rollback()
             db_cursor.close()
@@ -47,24 +47,26 @@ class OpsDb():
         print("ddl = ", ddl_sql.strip(strip_word))
         try:
             logger.debug("Execute DDL = {}".format(ddl_sql))
-            # db_cursor.execute(ddl_sql.strip(strip_word))
+            db_cursor.execute(ddl_sql.strip(strip_word))
         except Exception as err:
-            logger.error("Execute DDL [{}] statment get error.".format(ddl_sql))
+            logger.error("Execute DDL statement [{}] get error.".format(ddl_sql))
             logger.error("error info:\n{}".format(err))
             db_cursor.close()
             exit()
 
     def select_data(self, db_con_instance, select_sql, strip_word=';'):
         db_cursor = db_con_instance.cursor()
+        total_count = 0
         try:
             db_cursor.execute(select_sql.strip(strip_word))
             select_result = db_cursor.fetchall()
+            total_count=db_cursor.rowcount
         except Exception as err:
-            logger.error("Execute SELECT [{}] statment get error.".format(select_sql))
+            logger.error("Execute SELECT statement [{}] get error.".format(select_sql))
             logger.error("error info:\n{}".format(err))
             db_cursor.close()
             exit()
-        return select_result
+        return select_result,total_count
 
     def export_data_from_table(self, table_name):
         pass
@@ -86,8 +88,8 @@ class OpsDb():
         for drop_type in sql_cfg['drop_obj']:
             for onesql in sql_cfg['drop_obj'][drop_type]:
                 print("Process drop_type", drop_type)
-                logger.info("Clean DB_type {}".format(drop_type))
-                db_result_rows = self.select_data(self.cx_con, onesql)
+                db_result_rows,total_rows = self.select_data(self.cx_con, onesql)
+                logger.info("Clean DB_type {},Total counts {}".format(drop_type,total_rows))
                 if db_result_rows:
                     for row in db_result_rows:
                         # print ("row=",'.'.join(row))
